7446254f674b75415a0deedc13d7f71c160e34a1
Matias Vasconez <mvasconez1@outlook.com>
2023-04-08 10:18:50 -0500

    Part 2

---
+++ src/app/part2/part2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-part2',
+  templateUrl: './part2.component.html',
+  styleUrls: ['./part2.component.css']
+})
+export class Part2Component {
+  username = '';
+}
